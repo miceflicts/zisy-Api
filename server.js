bc3f1f2a998ce3fbd6927e02219259c8e2c8fe17
miceflicts <nickzinhomice@gmail.com>
2024-02-11 16:49:43 -0300

    :sparkles: feat: Implemented MongoDB

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require("express")
+const mongoose = require("mongoose")
 const app = express()
 
 // routes
-
 app.get("/", (req, res) => {
     res.send("Hello NODE API")
 })
 
-app.listen(3000, () => {
-    console.log("Node API APP is running on port 3000")
+app.get("/books", (req, res) => {
+    res.send("Hello NODE BOOKS")
 })
+
+mongoose.connect("mongodb+srv://admin:miceflicts123@zisyapi.zqvzvi5.mongodb.net/Node-API?retryWrites=true&w=majority")
+    .then(() => {
+        app.listen(3000, () => {
+            console.log("Node API APP is running on port 3000")
+        })
+    }).catch((err) => {
+        console.log(err)
+    })
